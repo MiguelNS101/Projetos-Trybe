942bfe68fc4b29c143f806d674199127b12426c0
MiguelNS <MiguelNS101@gmail.com>
2023-01-11 12:37:52 -0300

    Stranger Things Backend

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+require('dotenv/config');
+const cors = require('cors');
+// miguel
+const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
+const StrangerThingsRepository = require('./data/repository/StrangerThings');
+const StrangerThingsService = require('./services/StrangerThings');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+const strangerThingsRepository = new StrangerThingsRepository(
+  strangerThingsDataset,
+);
+const strangerThingsService = new StrangerThingsService(
+  strangerThingsRepository,
+);
+
+app.use(cors());
+
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
+
+app.get('/', (req, res) => {
+  const characters = strangerThingsService.search(
+    req.query,
+    hereIsTheUpsideDown,
+  );
+
+  res.status(200).json(characters);
+});
+
+app.listen(PORT, () => {
+  console.log('Escutando na porta 3000');
+});
